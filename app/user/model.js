7c809c130992dc32e6e54f0816842207ea2e8db9
Nathan Bland <nathan.bland@gmail.com>
2016-06-23 15:59:55 -0600

    enabling the user model again

--- app/user/model.js
+++ app/user/model.js
@@ -1,10 +1,10 @@
 import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
+import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-  // firstName: attr('string'),
-  // lastName: attr('string'),
-  // email: attr('string'),
-  // password: attr('string')  
+  firstName: attr('string'),
+  lastName: attr('string'),
+  email: attr('string'),
+  password: attr('string')  
 });
