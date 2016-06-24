2721b702ef6b208564b97587a21b6bd5ccb4894e
Nathan Bland <nathan.bland@gmail.com>
2016-06-24 15:48:18 -0600

    models and things

--- app/page/model.js
+++ app/page/model.js
@@ -1,7 +1,13 @@
 import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
+import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-
+  title: attr('string'),
+  content: attr('string'),
+  // author:  //not sure how to do this one yet, but a link to a user ID,
+  created: attr('date'),
+  lastUpdated: attr('date'),
+  comments: hasMany('comments'),
+  tags: hasMany('tags')
 });
